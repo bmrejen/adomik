d656ad18edaec22e8f06b9a4eb8cadf845874226
Benoit Mrejen <benoit.mrejen@eridanis.com>
2021-11-18 06:45:02 +0100

    edit url

--- src/utils.js
+++ src/utils.js
@@ -24,7 +24,7 @@ export function getData(key, setState) {
 }
 
 function _getUrlFromKey(key) {
-  return process.env.BACKEND_URL + key
+  return `${process.env.BACKEND_URL}/${key}`
 }
 
 function _fetchData(key, setState) {
