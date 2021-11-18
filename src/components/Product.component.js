2581a3743aafb4b525c0eab79bc200338610cd21
Benoit Mrejen <benoit.mrejen@eridanis.com>
2021-11-18 06:04:48 +0100

    remove old product component

--- src/components/Product.component.js
+++
@@ -1,9 +0,0 @@
-export default function Product({ product }) {
-  const citiesJsx = product.cities.map((city, i) => <span key={i}>{city.label} </span>)
-  return (
-    <>
-      <h1>Code: {product.code}</h1>
-      <p>Cities: {citiesJsx}</p>
-    </>
-  )
-}
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Product } from "./Product.component"
 export { default as ProductList } from "./ProductList.component"
 export { default as UserList } from "./UserList.component"
 export { default as DataTable } from "./DataTable.component"
